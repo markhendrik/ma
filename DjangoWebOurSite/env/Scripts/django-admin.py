9659294f5e01819f60c0a9fc65c378653e917050
markhendrik <mark.maeste@gmail.com>
2015-06-06 15:58:42 +0300

    Second

---
+++ DjangoWebOurSite/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\MarkH\Documents\Visual Studio 2012\Projects\DjangoWebOurSite\DjangoWebOurSite\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
